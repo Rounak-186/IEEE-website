eb202e046e2ee6634d227f756c9492fd3738c208
Sibsankar-de <sibsankarde910@gmail.com>
2025-10-24 19:05:56 +0530

    feat: home page text position correction

--- src/components/ui/sectionAnimation.tsx
+++ src/components/ui/sectionAnimation.tsx
@@ -1,12 +1,13 @@
 import { motion } from "framer-motion";
 
-export const SlideUpAnimation = ({ children }: { children: React.ReactNode }) => {
+export const SlideUpAnimation = ({ children, className = "" }: { children: React.ReactNode, className: string }) => {
     return (
         <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.3 }}
             transition={{ duration: 1, ease: "easeOut" }}
+            className={className}
         >
             {children}
         </motion.div>
